c8a95e6eb1a7137237a6cee28a1aed1bfb5374b2
ATE168Forever <ate168forever@gmail.com>
2025-08-23 13:45:32 +0800

    local modify

--- src/config.js
+++ src/config.js
@@ -1,2 +1,1 @@
-export const API_HOST = import.meta.env.VITE_API_HOST || 'http://127.0.0.1:8001';
-// export const API_HOST = "https://your-cloud-run-service.a.run.app";
+export const API_HOST = import.meta.env.VITE_API_HOST;
