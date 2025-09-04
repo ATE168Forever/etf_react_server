78c88edc3af857eb6ddc40ccaf23dfcfb1ef0308
ATE168Forever <127966043+ATE168Forever@users.noreply.github.com>
2025-09-04 19:20:19 +0800

    feat: add fintech landing component

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import StockDetail from './StockDetail.jsx'
+import FintechLanding from './FintechLanding.jsx'
 
 const path = window.location.pathname;
 const match = path.match(/^\/stock\/(\w+)/);
 const stockId = match ? match[1] : null;
+const showFintech = path === '/fintech';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {stockId ? <StockDetail stockId={stockId} /> : <App />}
+    {stockId ? <StockDetail stockId={stockId} /> : showFintech ? <FintechLanding /> : <App />}
   </StrictMode>,
 )
