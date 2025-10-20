317fe98de89860b63301b33cb8140088e16b724d
ATE168Forever <127966043+ATE168Forever@users.noreply.github.com>
2025-10-21 00:44:07 +0800

    refactor: remove firebase data sync support

--- apps/etf-life/jest.config.js
+++ apps/etf-life/jest.config.js
@@ -7,8 +7,6 @@ export default {
     '^.+\\.(js|jsx)$': 'babel-jest'
   },
   moduleNameMapper: {
-    '\\.(css|scss)$': '<rootDir>/test-style-mock.js',
-    '^firebase/app$': '<rootDir>/tests/mocks/firebaseApp.js',
-    '^firebase/firestore$': '<rootDir>/tests/mocks/firebaseFirestore.js'
+    '\\.(css|scss)$': '<rootDir>/test-style-mock.js'
   }
 };
--- apps/etf-life/tests/mocks/firebaseFirestore.js
+++
@@ -1,8 +0,0 @@
-import { jest } from '@jest/globals';
-
-export const getFirestore = jest.fn(() => ({ db: 'mock-db' }));
-export const doc = jest.fn(() => ({ id: 'mock-doc' }));
-export const setDoc = jest.fn(() => Promise.resolve());
-export const getDoc = jest.fn(() => Promise.resolve({ exists: () => false }));
-
-export default { getFirestore, doc, setDoc, getDoc };
